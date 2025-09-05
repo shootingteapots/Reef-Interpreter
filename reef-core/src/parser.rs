7a7bf4796f33609fb76f120628158ca945769063
shootingteapots <aaron.hughes0707@gmail.com>
2025-09-05 10:58:02 +0100

    i have no idea what im doing with this

--- reef-core/src/parser.rs
+++ reef-core/src/parser.rs
@@ -23,20 +23,35 @@ impl Parser {
     }
 
     pub fn parse_tokens(&mut self) -> ParseNode {
-        let base_node = ParseNode::new(ParseNodeKind::Program);
+        let mut base_node = ParseNode::new(ParseNodeKind::Program);
 
         while self.token_stream.len() > 0 {
-            let current_token = self.token_stream.get(self.current_token).unwrap();
+            let next_node = self.parse_expr();
 
-            self.parse_expr();
+            base_node.add_child(next_node);
         }
 
         base_node
     }
 
+    /// Base method for parsing any type of expression. Gets the next token from the
+    /// stream and matches it, calling the relevant function and appending the returned
+    /// node to the return node.
+    fn parse_expr(&mut self) -> ParseNode {
+        let next = self.get_next_token();
+
+        match *next {
+            Token::Number(number) => {
+                self.parse_number_expr(&number);
+            },
+            _ => {}
+        }
+
+        ParseNode::new(ParseNodeKind::Product)
+    }
+
     fn parse_number_expr(&mut self, number: &f64) -> ParseNode {
         let node = ParseNode::new(ParseNodeKind::Number(*number));
-        self.get_next_token();
 
         node
     }
@@ -56,19 +71,6 @@ impl Parser {
     }
 
     fn parse_ident_expr(&mut self) -> ParseNode {
-        ParseNode::new(ParseNodeKind::Product)
-    }
-
-    /// Base method for parsing any type of expression. Gets the next token from the
-    /// stream and matches it, calling the relevant function and appending the returned
-    /// node to the return node.
-    fn parse_expr(&mut self) -> ParseNode {
-        let next = self.get_next_token();
-
-        match next {
-            _ => {}
-        }
-
         ParseNode::new(ParseNodeKind::Product)
     }
 
